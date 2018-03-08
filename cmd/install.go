5dfb806e6e467bab3c89c34d5ff9e88e1ff7a271
mritd <mritd1234@gmail.com>
2018-03-08 23:10:24 +0800

    feat(0.0.1): 增加安装命令
    完善工具安装，目前仅支持 unix 系列系统
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- cmd/install.go
+++ cmd/install.go
@@ -21,32 +21,20 @@
 package cmd
 
 import (
-	"fmt"
-
+	"github.com/mritd/gitflow-toolkit/pkg/util"
 	"github.com/spf13/cobra"
 )
 
-// installCmd represents the install command
 var installCmd = &cobra.Command{
 	Use:   "install",
 	Short: "安装 gitflow-toolkit 工具",
 	Long: `
 针对不同平台自动安装 gitflow-toolkit 工具`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("install called")
+		util.Install()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(installCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// installCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// installCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- pkg/util/install.go
+++ pkg/util/install.go
@@ -1,1 +1,59 @@
 package util
+
+import (
+	"fmt"
+	"github.com/mitchellh/go-homedir"
+	"io"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+)
+
+func Install() {
+
+	home, err := homedir.Dir()
+	CheckAndExit(err)
+
+	toolHome := home + string(filepath.Separator) + ".gitflow-toolkit"
+	dstPath := toolHome + string(filepath.Separator) + "gitflow-toolkit"
+	hooksPath := toolHome + string(filepath.Separator) + "hooks"
+	commitMessageHookPath := hooksPath + string(filepath.Separator) + "commit-msg"
+	ciPath := "/usr/local/bin/git-ci"
+
+	currentPath, err := exec.LookPath(os.Args[0])
+	CheckAndExit(err)
+
+
+	if runtime.GOOS == "linux" || runtime.GOOS == "darwin" {
+
+		fmt.Println("Clean old files")
+		CheckAndExit(os.RemoveAll(toolHome))
+		CheckAndExit(os.Remove(ciPath))
+
+		fmt.Println("Create install home dir")
+		CheckAndExit(os.MkdirAll(toolHome, 0755))
+
+		fmt.Println("Copy file to home dir")
+		currentFile, err := os.Open(currentPath)
+		CheckAndExit(err)
+		dstFile, err := os.OpenFile(dstPath, os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0755)
+		CheckAndExit(err)
+		io.Copy(dstFile, currentFile)
+		//CheckAndExit(os.Chmod(dstPath,0755))
+
+		fmt.Println("Create symbolic file")
+		CheckAndExit(os.MkdirAll(hooksPath, 0755))
+		CheckAndExit(os.Symlink(dstPath, ciPath))
+		CheckAndExit(os.Symlink(dstPath, commitMessageHookPath))
+
+		fmt.Println("Config git")
+		CheckAndExit(exec.Command("git", "config", "--global", "--unset","core.hooksPath").Run())
+		CheckAndExit(exec.Command("git", "config", "--global", "core.hooksPath", hooksPath).Run())
+
+		fmt.Println("Well done.")
+
+	}
+}
+
+

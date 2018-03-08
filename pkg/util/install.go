b80c76ed03d6da04ba5463d22103ce6926c9cffa
mritd <mritd1234@gmail.com>
2018-03-09 00:47:26 +0800

    feat(0.0.1): 尝试兼容 windows
    尝试兼容 windows
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- pkg/util/install.go
+++ pkg/util/install.go
@@ -19,26 +19,31 @@ func Install() {
 	dstPath := toolHome + string(filepath.Separator) + "gitflow-toolkit"
 	hooksPath := toolHome + string(filepath.Separator) + "hooks"
 	commitMessageHookPath := hooksPath + string(filepath.Separator) + "commit-msg"
-	ciPath := "/usr/local/bin/git-ci"
 
 	currentPath, err := exec.LookPath(os.Args[0])
 	CheckAndExit(err)
 
 	if runtime.GOOS == "linux" || runtime.GOOS == "darwin" {
 
+		ciPath := "/usr/local/bin/git-ci"
+
 		fmt.Println("Clean old files")
-		CheckAndExit(os.RemoveAll(toolHome))
-		CheckAndExit(os.Remove(ciPath))
+		os.RemoveAll(toolHome)
+		os.Remove(ciPath)
 
 		fmt.Println("Create install home dir")
 		CheckAndExit(os.MkdirAll(toolHome, 0755))
 
-		fmt.Println("Copy file to home dir")
+		fmt.Println("Copy file to install home")
 		currentFile, err := os.Open(currentPath)
+		defer currentFile.Close()
 		CheckAndExit(err)
+
 		dstFile, err := os.OpenFile(dstPath, os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0755)
+		defer dstFile.Close()
 		CheckAndExit(err)
-		io.Copy(dstFile, currentFile)
+		_,err = io.Copy(dstFile, currentFile)
+		CheckAndExit(err)
 
 		fmt.Println("Create symbolic file")
 		CheckAndExit(os.MkdirAll(hooksPath, 0755))
@@ -46,10 +51,62 @@ func Install() {
 		CheckAndExit(os.Symlink(dstPath, commitMessageHookPath))
 
 		fmt.Println("Config git")
-		CheckAndExit(exec.Command("git", "config", "--global", "--unset", "core.hooksPath").Run())
+		exec.Command("git", "config", "--global", "--unset", "core.hooksPath").Run()
 		CheckAndExit(exec.Command("git", "config", "--global", "core.hooksPath", hooksPath).Run())
 
 		fmt.Println("Well done.")
 
+	} else if runtime.GOOS == "windows" {
+
+		ciPath := toolHome +string(filepath.Separator) + "git-ci.exe"
+
+		fmt.Println("Clean old files")
+		os.RemoveAll(toolHome)
+		os.Remove(ciPath)
+
+		fmt.Println("Create install home dir")
+		CheckAndExit(os.MkdirAll(toolHome, 0755))
+
+		fmt.Println("Copy file to install home")
+		currentFile, err := os.Open(currentPath)
+		defer currentFile.Close()
+		CheckAndExit(err)
+
+		dstFile, err := os.OpenFile(dstPath, os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0755)
+		defer dstFile.Close()
+		CheckAndExit(err)
+		_,err = io.Copy(dstFile, currentFile)
+		CheckAndExit(err)
+		currentFile.Seek(0, 0)
+
+		CheckAndExit(os.MkdirAll(hooksPath, 0755))
+		commitMessageHookFile,err :=os.OpenFile(commitMessageHookPath+".exe", os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0755)
+		defer commitMessageHookFile.Close()
+		_,err = io.Copy(commitMessageHookFile, currentFile)
+		CheckAndExit(err)
+		currentFile.Seek(0, 0)
+
+		ciFile,err := os.OpenFile(ciPath, os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0755)
+		CheckAndExit(err)
+		defer ciFile.Close()
+		_,err = io.Copy(ciFile, currentFile)
+		CheckAndExit(err)
+		currentFile.Seek(0, 0)
+
+
+		fmt.Println("Config git")
+		exec.Command("git", "config", "--global", "--unset", "core.hooksPath").Run()
+		CheckAndExit(exec.Command("git", "config", "--global", "core.hooksPath", hooksPath).Run())
+
+		fmt.Println("Config env")
+		winPath :=os.Getenv("Path")
+		newPath := toolHome
+		if winPath != ""{
+			newPath +=";" +winPath
+		}
+		CheckAndExit(exec.Command("SETX","Path",newPath).Run())
+
+
+		fmt.Println("Well done.")
 	}
 }

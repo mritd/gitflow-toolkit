d09801734d81126d7fcba6fbcb2ebd1b62f7c1e8
mritd <mritd1234@gmail.com>
2018-03-08 14:10:07 +0800

    fix(0.0.1): 修复全局替换错误
    修正全局替换时错误替换了配置文件相关变量
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- cmd/root.go
+++ cmd/root.go
@@ -40,7 +40,7 @@ var rootCmd = &cobra.Command{
 change log 生成等功能`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	Run: func(cmd *cobra.Command, args []string) {},
+	//Run: func(cmd *cobra.Command, args []string) {},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -58,7 +58,7 @@ func init() {
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "consts", "", "consts file (default is $HOME/.gitflow-toolkit.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gitflow-toolkit.yaml)")
 }
 
 // initConfig reads in consts file and ENV variables if set.
@@ -83,6 +83,6 @@ func initConfig() {
 
 	// If a consts file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using consts file:", viper.ConfigFileUsed())
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
 	}
 }

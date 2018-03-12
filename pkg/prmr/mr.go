ef8d62a07eac1f38a05cd41ac0fe55378b598c1e
mritd <mritd1234@gmail.com>
2018-03-12 18:53:16 +0800

    feat(0.0.1): 增加 mr 指令
    增加 mr 指令
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- pkg/prmr/mr.go
+++ pkg/prmr/mr.go
@@ -5,8 +5,6 @@ import (
 
 	"strings"
 
-	"fmt"
-
 	"os"
 	"path/filepath"
 
@@ -25,9 +23,10 @@ type Repository struct {
 	Type        consts.RepoType
 	Address     string
 	Path        string
+	AutoRebase  bool
 }
 
-func GetRepository() *Repository {
+func ConfigRepository() *Repository {
 
 	repo := Repository{}
 
@@ -46,7 +45,7 @@ func GetRepository() *Repository {
 	}
 
 	prompt := promptui.Prompt{
-		Label:     "❯ Project(eg: gitflow-toolkit):",
+		Label:     "❯ Project(eg: namespace/project):",
 		Templates: templates,
 		Validate:  validate,
 	}
@@ -90,12 +89,39 @@ func GetRepository() *Repository {
 	} else {
 		repo.Type = consts.GitLabRepo
 	}
-	repo.Path = util.CurrentDir
+
+	validate = func(input string) error {
+		if strings.TrimSpace(input) == "" {
+			return errors.New("subject is blank")
+		}
+		if strings.ToLower(strings.TrimSpace(input)) != "y" && strings.ToLower(strings.TrimSpace(input)) != "n" {
+			return errors.New("only enter y or n")
+		}
+		return nil
+	}
+
+	prompt = promptui.Prompt{
+		Label:     "❯ Automatic rebase before merge request?(y/n)",
+		Templates: templates,
+		Validate:  validate,
+		IsVimMode: true,
+	}
+
+	autoRebase, err := prompt.Run()
+	util.CheckAndExit(err)
+
+	if strings.ToLower(strings.TrimSpace(autoRebase)) == "y" {
+		repo.AutoRebase = true
+	} else {
+		repo.AutoRebase = false
+	}
+
+	repo.Path = util.WorkingDir
 
 	return &repo
 }
 
-func SaveRepository(repository *Repository) {
+func (repo *Repository) SaveRepository() {
 
 	home, err := homedir.Dir()
 	util.CheckAndExit(err)
@@ -108,9 +134,9 @@ func SaveRepository(repository *Repository) {
 	var repositories []*Repository
 
 	util.CheckAndExit(viper.UnmarshalKey("repositories", &repositories))
-	repositories = append(repositories, repository)
+	repositories = append(repositories, repo)
 	viper.Set("repositories", repositories)
-	fmt.Println(viper.WriteConfig())
+	util.CheckAndExit(viper.WriteConfig())
 
 }
 
@@ -120,23 +146,83 @@ func GetRepoInfo() *Repository {
 	util.CheckAndExit(viper.UnmarshalKey("repositories", &repositories))
 
 	for _, repo := range repositories {
-		if repo.Path == util.CurrentDir {
+		if repo.Path == util.WorkingDir {
 			return repo
 		}
 	}
 	return nil
 }
 
-func Mr(repo *Repository) {
+func (repo *Repository) Mr() {
 	git := gitlab.NewClient(nil, repo.Token)
 	git.SetBaseURL(repo.Address + "/api/v3")
 	p, _, err := git.Projects.GetProject(repo.Project)
 	util.CheckAndExit(err)
-	opt := &gitlab.CreateMergeRequestOptions{
-		Title:        gitlab.String(""),
-		Description:  gitlab.String(""),
-		SourceBranch: gitlab.String(""),
-		TargetBranch: gitlab.String(""),
+
+	lastCommitInfo := *util.GetLastCommitInfo()
+
+	validate := func(input string) error {
+		if strings.TrimSpace(input) == "" {
+			return errors.New("target branch is blank")
+		}
+		return nil
 	}
-	git.MergeRequests.CreateMergeRequest(p.ID, opt)
+
+	templates := &promptui.PromptTemplates{
+		Prompt:  "{{ . }} ",
+		Valid:   "{{ . | green }} ",
+		Invalid: "{{ . | red }} ",
+		Success: "{{ . | bold }} ",
+	}
+
+	prompt := promptui.Prompt{
+		Label:     "❯ Title(eg: fix(v0.0.1):fix a bug):",
+		Templates: templates,
+	}
+
+	title, err := prompt.Run()
+	util.CheckAndExit(err)
+	if strings.TrimSpace(title) == "" {
+		title = lastCommitInfo[0]
+	}
+
+	prompt = promptui.Prompt{
+		Label:     "❯ Description(eg: Rollback etcd server version to 3.1.11):",
+		Templates: templates,
+	}
+
+	desc, err := prompt.Run()
+	util.CheckAndExit(err)
+
+	if strings.TrimSpace(desc) == "big" {
+		desc = util.OSEditInput()
+	}
+
+	if strings.TrimSpace(desc) == "" {
+		desc = lastCommitInfo[1]
+	}
+
+	prompt = promptui.Prompt{
+		Label:     "❯ Target Branch(eg: develop):",
+		Templates: templates,
+		Validate:  validate,
+	}
+
+	targetBranch, err := prompt.Run()
+	util.CheckAndExit(err)
+
+	sourceBranch := util.GetCurrentBranch()
+
+	if repo.AutoRebase {
+		util.Rebase(sourceBranch, targetBranch)
+	}
+
+	opt := &gitlab.CreateMergeRequestOptions{
+		Title:        gitlab.String(title),
+		Description:  gitlab.String(desc),
+		SourceBranch: gitlab.String(sourceBranch),
+		TargetBranch: gitlab.String(targetBranch),
+	}
+	_, _, err = git.MergeRequests.CreateMergeRequest(p.ID, opt)
+	util.CheckAndExit(err)
 }
--- pkg/util/common.go
+++ pkg/util/common.go
@@ -1,14 +1,15 @@
 package util
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"runtime"
 
 	"os/user"
-
-	"strings"
 
 	"github.com/mitchellh/go-homedir"
 )
@@ -19,6 +20,7 @@ var HooksPath string
 var GitCMHookPath string
 var CurrentPath string
 var CurrentDir string
+var WorkingDir string
 var GitCIPath = "/usr/local/bin/git-ci"
 
 func init() {
@@ -35,9 +37,10 @@ func init() {
 
 	CurrentPath, err = exec.LookPath(os.Args[0])
 	CheckAndExit(err)
-	absPath, err := filepath.Abs(CurrentPath)
+	CurrentDir, err = filepath.Abs(filepath.Dir(os.Args[0]))
 	CheckAndExit(err)
-	CurrentDir = strings.Replace(absPath, "\\", "/", -1)
+	WorkingDir, err = os.Getwd()
+	CheckAndExit(err)
 }
 
 func CheckErr(err error) bool {
@@ -85,4 +88,39 @@ func CheckRoot() {
 		fmt.Println("This command must be run as root! (sudo)")
 		os.Exit(1)
 	}
+}
+
+func OSEditInput() string {
+
+	f, err := ioutil.TempFile("", "gitflow-toolkit")
+	CheckAndExit(err)
+	defer os.Remove(f.Name())
+
+	// write utf8 bom
+	bom := []byte{0xef, 0xbb, 0xbf}
+	_, err = f.Write(bom)
+	CheckAndExit(err)
+
+	// 获取系统编辑器
+	editor := "vim"
+	if runtime.GOOS == "windows" {
+		editor = "notepad"
+	}
+	if v := os.Getenv("VISUAL"); v != "" {
+		editor = v
+	} else if e := os.Getenv("EDITOR"); e != "" {
+		editor = e
+	}
+
+	// 执行编辑文件
+	cmd := exec.Command(editor, f.Name())
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	CheckAndExit(cmd.Run())
+	raw, err := ioutil.ReadFile(f.Name())
+	CheckAndExit(err)
+	input := string(bytes.TrimPrefix(raw, bom))
+
+	return input
 }

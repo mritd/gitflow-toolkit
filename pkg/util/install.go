fb80885949075cb51ff2e7a02c96fcb4e8ecbd04
mritd <mritd1234@gmail.com>
2018-06-21 11:35:02 +0800

    feat(uninstall): add os check
    add os check
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- pkg/util/install.go
+++ pkg/util/install.go
@@ -4,42 +4,36 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"runtime"
 )
 
 func Install() {
 
-	if runtime.GOOS == "linux" || runtime.GOOS == "darwin" {
+	Uninstall()
 
-		Uninstall()
+	fmt.Println("Install gitflow-toolkit")
+	fmt.Println("Create install home dir")
+	CheckAndExit(os.MkdirAll(GitFlowToolKitHome, 0755))
 
-		fmt.Println("Install gitflow-toolkit")
-		fmt.Println("Create install home dir")
-		CheckAndExit(os.MkdirAll(GitFlowToolKitHome, 0755))
+	fmt.Println("Copy file to install home")
+	currentFile, err := os.Open(CurrentPath)
+	CheckAndExit(err)
+	defer currentFile.Close()
 
-		fmt.Println("Copy file to install home")
-		currentFile, err := os.Open(CurrentPath)
-		defer currentFile.Close()
-		CheckAndExit(err)
+	installFile, err := os.OpenFile(InstallPath, os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0755)
+	CheckAndExit(err)
+	defer installFile.Close()
 
-		installFile, err := os.OpenFile(InstallPath, os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0755)
-		defer installFile.Close()
-		CheckAndExit(err)
-		_, err = io.Copy(installFile, currentFile)
-		CheckAndExit(err)
+	_, err = io.Copy(installFile, currentFile)
+	CheckAndExit(err)
 
-		fmt.Println("Create symbolic file")
-		CheckAndExit(os.MkdirAll(HooksPath, 0755))
+	fmt.Println("Create symbolic file")
+	CheckAndExit(os.MkdirAll(HooksPath, 0755))
 
-		for _, binPath := range *BinPaths() {
-			CheckAndExit(os.Symlink(InstallPath, binPath))
-		}
-
-		fmt.Println("Config git")
-		MustExec("git", "config", "--global", "core.hooksPath", HooksPath)
-
-	} else {
-		fmt.Println("Platform not support!")
-		os.Exit(1)
+	for _, binPath := range *BinPaths() {
+		CheckAndExit(os.Symlink(InstallPath, binPath))
 	}
+
+	fmt.Println("Config git")
+	MustExec("git", "config", "--global", "core.hooksPath", HooksPath)
+
 }
